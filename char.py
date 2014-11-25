b98a38318a6272bcb7698a8365122d3addfad0bb
Matt T <mstomasze@aol.com>
2014-11-25 15:58:18 -0500

    lots of indents and stuff
    god speed michael

--- char.py
+++ char.py
@@ -1,8 +1,16 @@
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.graphics import Rectangle, Ellipse, Line
 import time
 
 class Char(Widget):
     def __init__(self):
-        super(StartScreen, self).__init__()
+        super(Char, self).__init__()
         self.layout = FloatLayout()
         self.sprite = Image(
             source="assets/morgan-freeman.jpg",
@@ -10,18 +18,16 @@ class Char(Widget):
             size=(30, 50),
             #center_x=Window.width / 2,
             #center_y=(Window.height / 2) + 150
-
-        player = ObjectProperty(None)
-
-        def __init__(self, **kwargs):
+        )
+        #player = ObjectProperty(None)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
-
-        def _keyboard_closed(self):
+        self.layout.add_widget(self.sprite)
+    def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
 
-        def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[1] == 'd':
             self.player.center_x += 10
         elif keycode[1] == 'a':
@@ -31,5 +37,3 @@ class Char(Widget):
             time.sleep(.5)
             self.player.center_y -= 10
         return True
-        )
-        self.layout.add_widget(self.sprite)
